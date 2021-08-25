65ac761fa3b7eed97af042286dd8a744f655ff30
Nasim Ahamad Khan <40820072+NakRex@users.noreply.github.com>
2021-08-25 11:54:47 +0530

    updated

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-
-
-
 //create a submit button
-function myFunc(){
-    let text = document.getElementById("id1").value;
-    console.log(text);
-    var h1 = document.createElement("h1");
-    h1.innerHTML = text;
-    h1.className = "myclass";
-    document.body.insertBefore(h1, document.body.firstChild);
-    document.getElementById("id1").remove;
-    document.getElementById("id2").remove;
-   }
-
-
+function myFunc() {
+  let text = document.getElementById("id1").value;
+  console.log(text);
+  var h1 = document.createElement("h1");
+  h1.innerHTML = text;
+  h1.className = "myclass";
+  document.body.insertBefore(h1, document.body.firstChild);
+  document.getElementById("id1").hidden;
+  document.getElementById("id2").hidden;
+}
 
 var s = document.createElement("input");
 s.setAttribute("type", "button");
 // s.setAttribute("onclick", myFunc());
-s.addEventListener("click", myFunc)
+s.addEventListener("click", myFunc);
 s.setAttribute("value", "Try It");
 s.id = "id2";
 document.body.insertBefore(s, document.body.firstChild);
@@ -30,7 +25,3 @@ FN.setAttribute("name", "FullName");
 FN.setAttribute("placeholder", "Enter your Name");
 FN.id = "id1";
 document.body.insertBefore(FN, document.body.firstChild);
-    
-   
-
-    

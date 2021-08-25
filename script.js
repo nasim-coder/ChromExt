937a72f25559e639f4c96d1815e602876a20fe65
Nasim Ahamad Khan <40820072+NakRex@users.noreply.github.com>
2021-08-25 11:01:20 +0530

    chrome_extension

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+
+//create a submit button
+function myFunc(){
+    let text = document.getElementById("id1").value;
+    console.log(text);
+    var h1 = document.createElement("h1");
+    h1.innerHTML = text;
+    h1.className = "myclass";
+    document.body.insertBefore(h1, document.body.firstChild);
+    
+   }
+
+
+
+var s = document.createElement("input");
+s.setAttribute("type", "button");
+// s.setAttribute("onclick", myFunc());
+s.addEventListener("click", myFunc)
+s.setAttribute("value", "Try It");
+s.id = "id2";
+document.body.insertBefore(s, document.body.firstChild);
+
+//Create an input element for Full Name
+    var FN = document.createElement("input");
+    FN.setAttribute("type", "text");
+    FN.setAttribute("name", "FullName");
+    FN.setAttribute("placeholder", "Enter your Name");
+    FN.id = "id1";
+    document.body.insertBefore(FN, document.body.firstChild);
+    
+   
+
+    

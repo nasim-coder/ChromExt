506c57abcee78c0bedfb3621726df8a3baa22fa5
NakRex <khannasimahmad@gmail.com>
2021-12-13 19:23:30 +0530

    fixed date

--- myPage.js
+++ myPage.js
@@ -17,22 +17,21 @@ function showTheQuote() {
 	//returns the greeting as per the time
 	 function getGreetings(currentHours) {
 		if (currentHours <= 12) {
-			return "Good morning";
+			return "Good Morning";
 		}
 		else if (currentHours <= 18 && currentHours > 12) {
-			return "Good after noon";
+			return "Good After Noon";
 		} else {
-			return "Good evening";
+			return "Good Evening";
 		}
 	}
 
 	let today = new Date();
 	let currentHours = today.getHours();
 	let greeting = getGreetings(currentHours);
-	document.querySelector(".greeting").innerHTML = greeting + " Nasim, Hope you are doing well"
-
-
-	document.querySelector(".date").innerHTML=new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+	document.querySelector(".greeting").innerHTML = greeting + " Nasim"
+	document.querySelector(".time").innerHTML=new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+	document.querySelector('.date').innerHTML=new Date().toDateString();
 }
 //Once DOM is loaded run the showTheContent method 
 window.addEventListener('DOMContentLoaded', showTheQuote,);
